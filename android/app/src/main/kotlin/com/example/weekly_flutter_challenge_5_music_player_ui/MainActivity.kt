39ad7935eef28afe15a4953d20e30624091b2c1e
JKPK <jacek.kawiak@gmail.com>
2019-10-13 18:44:27 +0200

    Initial source code.

---
+++ android/app/src/main/kotlin/com/example/weekly_flutter_challenge_5_music_player_ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weekly_flutter_challenge_5_music_player_ui
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weekly Flutter Challenge 5",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
